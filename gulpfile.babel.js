5d27e463d0bf65a67817750583e1535a1666332c
massicollado <colladomassiel@gmail.com>
2019-06-25 12:40:13 -0600

    Minifica SCSS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const uglyfly = require('gulp-uglyfly');
+var uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 
 gulp.task('transpilajs', () =>
     gulp.src('src/js/*.js') //directorio origen
         .pipe(babel({
             presets: ['@babel/env'] //proceso de transpilacion
         }))
-        .pipe(uglyfly())
+        .pipe(uglify())
         .pipe(gulp.dest('dist')) //destino
 );
+
+gulp.task('minificahtml', () => {
+    return gulp.src('src/html/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
+
+  gulp.task('minificascss', function () {
+    return gulp.src('src/scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('dist'));
+  });
